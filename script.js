70fe2ed8284a93e4350d0d737b61af819ed20650
Prabhat Kumar <2012prabhat@gmail.com>
2022-05-09 09:26:51 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,95 @@
+let songName = document.querySelector(".songName");
+let selectSong = document.querySelector(".selectSong");
+let musicPlayer = document.querySelector(".musicPlayer");
+let songsCont = document.querySelector(".songsCont");
+let thumbnail = document.querySelector(".pic");
+let progress = document.querySelector(".progress");
+let playBtn = document.querySelector(".playBtn");
+let prevBtn = document.querySelector(".prevBtn");
+let nextBtn = document.querySelector(".nextBtn");
+let songDuration = document.querySelector(".songDuration");
+let currDuration = document.querySelector(".currDuration");
+let audioFlag = false;
+let audioElem = new Audio("songs/1.mp3");
+let cSongIdx = 0;
+songName.innerHTML = songs[0].songName;
+thumbnail.setAttribute("src",songs[0].coverPath)
+const handleSelectedSong = (i)=>{
+    if(i==songs.length) i = 0;
+    audioElem.pause();
+    audioElem = new Audio(songs[i].filePath);
+    audioElem.play();
+    audioFlag = true;
+    playBtn.innerHTML = "pause";
+    cSongIdx = i;
+    thumbnail.setAttribute("src",songs[i].coverPath);
+    songName.innerHTML = songs[i].songName;
+     
+    audioElem.addEventListener('timeupdate',()=>{
+        let progressVal = (audioElem.currentTime/audioElem.duration)*100;
+        progress.value = progressVal;
+        if(audioElem.currentTime == audioElem.duration){
+            handleSelectedSong(cSongIdx+1);
+        }
+        let min = Math.floor(parseInt(audioElem.duration)/60);
+        let sec = Math.floor(parseInt(audioElem.duration)%60);
+        let cmin = Math.floor(parseInt(audioElem.currentTime)/60);
+        let csec = Math.floor(parseInt(audioElem.currentTime)%60);
+        if(min<=9) min ="0"+min
+        if(cmin<=9) cmin = "0"+cmin;
+        if(csec<=9) csec = "0"+csec;
+        currDuration.innerHTML = cmin+":"+csec;
+        songDuration.innerHTML = min+":"+sec;
+    })
+
+    selectSong.classList.add("hidden");
+    musicPlayer.classList.remove("hidden");
+}
+progress.addEventListener("change",()=>{
+    audioElem.currentTime = (progress.value * audioElem.duration)/100;
+})
+prevBtn.addEventListener("click",()=>{
+    cSongIdx = cSongIdx-1;
+    if(cSongIdx==-1) cSongIdx=songs.length-1;
+    handleSelectedSong(cSongIdx);
+    
+})
+nextBtn.addEventListener("click",()=>{
+    cSongIdx = cSongIdx+1;
+    if(cSongIdx==songs.length) cSongIdx=0;
+    handleSelectedSong(cSongIdx);
+    
+})
+
+
+playBtn.addEventListener("click",()=>{
+if(!audioFlag){
+     audioElem.play();
+     audioFlag = !audioFlag;
+     playBtn.innerHTML = "pause"; 
+    }else{
+        audioElem.pause();
+        audioFlag = !audioFlag;
+        playBtn.innerHTML = "play_arrow"; 
+} 
+})
+songs.map((m,i)=>{
+    let songCover = document.createElement('div');
+    songCover.innerHTML = `<div class="relative border-2 border-black-900 bg-cover rounded md:w-[180px] w-[150px] hover:scale-110  ease-in-out duration-300 bg-full h-[25vh] m-2 bg-[url(${m.coverPath})] text-white flex items-end justify-center">
+    <Button class="material-icons rounded-3xl bg-black text-white" >play_arrow</Button>
+    <div class="text-sm text-center bg-black w-full py-1">${m.songName.slice(0,21)}</div></div>`
+    songsCont.append(songCover);
+    songCover.addEventListener("click",()=>handleSelectedSong(i));
+})
+
+
+const handlePrev = (m,i)=>{
+    audioElem.pause();
+    audioElem = new Audio(songs[i-1].filePath);
+    audioElem.play();
+    audioFlag = true;
+    playBtn.innerHTML = "pause";
+}
+
+
+
---
+++ songs.js
@@ -0,0 +1,29 @@
+let songs = [
+  {
+    songName: "Royality",
+    filePath: "https://sdlhivkcdnems02.cdnsrv.jio.com/jiosaavn.cdn.jio.com/955/a70ad882ccb4625360dafc371b821c5b_160.mp4",
+    coverPath: "https://c.saavncdn.com/955/Royalty-Unknown-2021-20210412192043-500x500.jpg",
+    language:"English"
+  },
+
+  {
+    songName: "Naina Mere",
+    filePath:
+      "https://sklktecdnems03.cdnsrv.jio.com/jiosaavn.cdn.jio.com/484/16328643d38d772fda86beeaf49eddb3_160.mp4",
+    coverPath:
+      "https://c.saavncdn.com/484/Naina-Mere-Hindi-2022-20220222131523-500x500.jpg",
+      language:"Hindi"
+  },
+  {
+      filePath:"https://sklktecdnems01.cdnsrv.jio.com/jiosaavn.cdn.jio.com/907/c4e7557f393b8a65bb617afd7bcf76a7_160.mp4",
+      songName:"Mitra Re - Runaway 34",
+      coverPath:"https://c.saavncdn.com/907/Mitra-Re-From-Runway-34--Hindi-2022-20220402164830-500x500.jpg",
+      language:"Hindi"
+  },
+  {
+      filePath:"https://sgdccdnems01.cdnsrv.jio.com/jiosaavn.cdn.jio.com/024/8b59f187279e635513d156ada5b36157_160.mp4",
+      songName:"Heeriye- Pyar Ka Punchnama-2",
+      coverPath:"https://c.saavncdn.com/024/Pyaar-Ka-Punchnama-2-Hindi-2018-20180517-500x500.jpg",
+      language:"Hindi"
+  }
+];
